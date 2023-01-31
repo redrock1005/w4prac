6e702817ab19b25dc2893ab9ef074fe9a51c2311
jungseok <jungseokoh1005@gmail.com>
2023-01-31 16:14:00 +0900

    ott이름 저장하도록 app.pyt 수정
    - ott.py에서 ott 이미지url을 추출할 수 있도록 테스트 중

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
@@ -19,34 +20,49 @@ def home():
 def test_post():
     url_give = request.form['url_give']
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_give, headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
+    driver = webdriver.Chrome(url_give)
+    # 암묵적으로 웹 지원 로드를 위해 3초까지 기다려준다.
+    driver.implicitly_wait(3)
+    # url에 접근한다.
+    driver.get("https://m.kinolights.com/title/111951")
+    html = driver.page_source  ##페이지의 element 모두 가져오기
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    # data = requests.get(url_give, headers=headers)
+    # soup = BeautifulSoup(data.text, 'html.parser')
 
     contents = soup.select('#contents > div.movie-info-container > div.movie-header-area')
     for content in contents:
         content_title = content.select_one('div > h3').text
         image_url = content.select_one('div > img')['src']
 
-    data = []
+    data_actor = []
     actors_name = soup.select('#synopsis > article:nth-child(7) > div > div')
     for actor_name in actors_name:
         actors = actor_name.select_one('div.name').text
         if actors is not None:
             actor = actors
-            data.append({'actor':actor})
+            data_actor.append({'actor':actor})
 
     Synops = soup.select('#synopsis > article:nth-child(1)')
     for Synop in Synops:
         synop = Synop.select_one('p').text
 
+    data_ott = []
+    otts_name = soup.select("#streamingVodList > div > div.price-item-provider > div.provider-info")
+    for ott_name in otts_name:
+        OTTs = ott_name.select_one('p').text
+        data_ott.append({'ott': OTTs})
+
 
     doc={
         'title':content_title,
         'image':image_url,
-         'actor':data,
+         'actor':data_actor,
         'synop':synop,
+        'ott':data_ott
     }
     db.w4prac.insert_one(doc)
     return jsonify({'msg': '저장 완료'})
--- ott.py
+++ ott.py
@@ -1,13 +1,28 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://m.kinolights.com/title/111951', headers=headers)
-soup = BeautifulSoup(data.text, 'html.parser')
+driver = webdriver.Chrome('/Users/jungseokoh/Downloads/chromedriver_mac64/chromedriver')
+#암묵적으로 웹 지원 로드를 위해 3초까지 기다려준다.
+driver.implicitly_wait(3)
+#url에 접근한다.
+driver.get("https://m.kinolights.com/title/111951")
+html = driver.page_source ##페이지의 element 모두 가져오기
+soup = BeautifulSoup(html, 'html.parser')
 
-price_provider = soup.select('.provider-info')
-for item in price_provider:
-    print(item)
+data_ott = []
+otts_name = soup.select("#streamingVodList > div > div.price-item-provider > div.provider-info")
+for ott_name in otts_name:
+    OTTs = ott_name.select_one('p').text
+    data_ott.append({'ott':OTTs})
+    print(data_ott)
 
-#dddddgit add .Wndtjrdh2@
+
+#OTT이미지는 어떻게 가져올지.
+
+# data_ott_image = []
+# otts_image = soup.select("#streamingVodList > div > div.price-item-provider > div.provider-info")
+# for ott_image in otts_image:
+#     OTTs_image = ott_image.select_one('p')
+#     data_ott_image.append({'ott_image':OTTs})
+#     print(data_ott_image)
+
