dbec5e45bc3b57f668936636a52ae7f904134c14
jungseok <jungseokoh1005@gmail.com>
2023-02-04 16:27:44 +0900

    app.py : 이미지(bg, poster)저장하도록 수정, 감독/배우 이름 저장하지 못하던 현상 수정, 년도 저장 추가

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
 import requests
@@ -7,18 +8,22 @@ from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
 import certifi
+from bson.objectid import ObjectId
+
 ca = certifi.where()
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.jl043qw.mongodb.net/?retryWrites=true&w=majority', tlsCAFile = ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.jl043qw.mongodb.net/?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
 db = client.dbsparta
 
 
-#크롤링할 페이지, indext.html
+# 크롤링할 페이지, indext.html
 @app.route('/')
 def home():
     return render_template('index.html')
 
-#크롤링api
+
+# 크롤링api
 @app.route('/test', methods=['POST'])
 def test_post():
     url_give = request.form['url_give']
@@ -55,7 +60,7 @@ def test_post():
         actors = actor_name.select_one('div.name')
         if actors is not None:
             actor = actors.text
-            data_actor.append({'actor':actor})
+            data_actor.append({'actor': actor})
 
     Synops = soup.select('#synopsis > article:nth-child(1)')
     for Synop in Synops:
@@ -72,36 +77,43 @@ def test_post():
         detail = year.select('span')
         year_content = detail[1].text
 
-    doc={
-        'title':content_title,
-        'bg_image':bg_url,
-        'poster_image':poster_url,
-         'actor':data_actor,
-        'synop':synop,
-        'ott':data_ott,
-        'year':year_content
+    doc = {
+        'title': content_title,
+        'bg_image': bg_url,
+        'poster_image': poster_url,
+        'actor': data_actor,
+        'synop': synop,
+        'ott': data_ott,
+        'year': year_content,
+        'comment_contents': []
+        # {'comment': '', 'star': '', 'time': ''}
     }
     db.w4prac_crwaler.insert_one(doc)
     return jsonify({'msg': '저장 완료'})
 
-#상세페이지, 한줄평저장api
-@app.route('/detail/comments', methods=['POST'])
-def detail_comment_post():
-    comment_give = request.form['comment_give']
-    star_give = request.form['comment_give']
-    time_give = request.form['comment_give']
 
-    comment_ott = {
-        'comment': comment_give,
-        'star': star_give,
-        'data': time_give
+# 상세페이지, 한줄평저장api
+@app.route('/detail/', methods=['POST'])
+def detail_comment_post():
+    # id_give = request.form['id_give']
+    id_give = '63dde3df663cfbf29a97d538'
+    comment_give = request.form['comment_give']
+    star_give = request.form['star_give']
+    time_give = request.form['time_give']
+
+    a = []
+    a.append({'comment':comment_give, 'star': star_give, 'time': time_give})
+
+    # db.w4prac_comments.insert_one(comment_content)
+
+    ret = db.w4prac_crwaler.update_one(
+        {"_id": ObjectId(id_give)},
+         { "$set":{'comment_contents': a}
     }
-    db.w4prac_comments.insert_one(comment_ott)
+    )
+    print(ret)
     return jsonify({'msg': '한줄평 등록 완료'})
 
 
-
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=4000,debug=True)
-
-
+    app.run('0.0.0.0', port=4000, debug=True)
